73c9af19feaf6ea3ddf6a04d86d37f79011fbb92
wuwu <15093565100@163.com>
2025-11-22 20:57:41 +0800

    fix

--- extension/tableprefix/tableprefix.go
+++ extension/tableprefix/tableprefix.go
@@ -2,9 +2,7 @@ package tableprefix
 
 import (
 	"embed"
-	"strings"
 
-	"entgo.io/ent/dialect/entsql"
 	"entgo.io/ent/entc"
 	"entgo.io/ent/entc/gen"
 )
@@ -17,8 +15,8 @@ type TablePrefixExtension struct {
 	entc.DefaultExtension
 }
 
-func NewTablePrefix() (*TablePrefixExtension, error) {
-	return &TablePrefixExtension{}, nil
+func NewTablePrefix() *TablePrefixExtension {
+	return &TablePrefixExtension{}
 }
 
 // Templates 返回用于代码生成的模板列表
@@ -32,49 +30,4 @@ func (e *TablePrefixExtension) Templates() []*gen.Template {
 	)
 
 	return []*gen.Template{tmpl}
-}
-
-type TablePrefixGenExtension struct {
-	entc.DefaultExtension
-	Prefix string
-}
-
-func NewTablePrefixGen(prefix string) (*TablePrefixGenExtension, error) {
-	return &TablePrefixGenExtension{Prefix: prefix}, nil
-}
-
-func (e *TablePrefixGenExtension) Hooks() []gen.Hook {
-	return []gen.Hook{
-		e.generateTablePrefix(e.Prefix),
-	}
-}
-
-func (e *TablePrefixGenExtension) generateTablePrefix(prefix string) gen.Hook {
-	return AddTablePrefix(prefix)
-}
-
-func AddTablePrefix(prefix string) gen.Hook {
-	return func(next gen.Generator) gen.Generator {
-		return gen.GenerateFunc(func(g *gen.Graph) error {
-
-			for _, n := range g.Nodes {
-				if n.Annotations == nil {
-					n.Annotations = make(map[string]any)
-				}
-
-				ann := &entsql.Annotation{}
-				if a := n.EntSQL(); a != nil {
-					*ann = *a
-				}
-				if ann.Table == "" {
-					ann.Table = prefix + strings.ToLower(n.Name)
-				}
-				n.Annotations[ann.Name()] = ann
-			}
-			if err := next.Generate(g); err != nil {
-				return err
-			}
-			return nil
-		})
-	}
 }
